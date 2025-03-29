036d544be828e1c601b7fa1e8e0ba836d65e04d3
JobLessGod <iamjoblessgod@gmail.com>
2025-03-30 00:41:52 +0545

    feat: add slash command and activity status for the Discord bot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv/config');
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, Collection } = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -10,8 +10,34 @@ const client = new Client({
     ],
 });
 
-client.on('ready', (client) => {
+client.on('ready', async () => {
     console.log(`${client.user.username} is online`);
+    client.user.setActivity({
+        name: "🤑 Developer Sanchit",
+        type: ActivityType.Custom
+    })
+
+    await client.application.commands.create({
+        name: 'sanchit',
+        description: 'Replies with Sanchit\'s bot message.',
+    });
+    console.log('Slash command registered!');
+
 });
+
+client.on('messageCreate', (message) => {
+    if (message.content === '-sanchit') {
+        message.reply('Hello, I am <:Developer:1355613770394767410> Sanchit\'s bot!');
+    }
+});
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'sanchit') {
+        interaction.reply('Hello, I am <:Developer:1355613770394767410> Sanchit\'s bot!');
+    }
+});
+
 
 client.login(process.env.TOKEN);
